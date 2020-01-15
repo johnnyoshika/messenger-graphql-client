b8a3f1aa313b34803c5da04544e6119bf391a130
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-15 08:56:41 -0800

    Add ChannelsList to App

--- src/App.js
+++ src/App.js
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const ChannelsList = () => (
+  <ul className="Item-list">
+    <li>Channel 1</li>
+    <li>Channel 2</li>
+  </ul>
+);
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to Messenger</h2>
+    </header>
+    <ChannelsList />
+  </div>
+);
 
 export default App;
