da58e41b1c837dbdc96d73a0c708a327df47560e
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-15 09:39:00 -0800

    Retrieve channels from GraphQL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_CHANNELS } from './queries';
+
+import Error from './Error';
+import Loading from './Loading';
+
 import logo from './logo.svg';
 import './App.css';
 
-const ChannelsList = () => (
-  <ul className="Item-list">
-    <li>Channel 1</li>
-    <li>Channel 2</li>
-  </ul>
-);
+
+const ChannelsList = () => {
+  const { data, loading, error } = useQuery(GET_CHANNELS);
+
+  if (error) return <Error error={error} />;
+
+  if (loading && !data) return <Loading />;
+
+  const { channels } = data;
+
+  return (
+    <ul className="Item-list">
+      {channels.map(channel => (
+        <li key={channel.id}>{channel.name}</li>
+      ))}
+    </ul>
+  );
+}
 
 const App = () => (
   <div className="App">
