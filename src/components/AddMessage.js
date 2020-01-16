d59f8912afa550f64c0c600c6e39604859eb6f69
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-15 17:31:01 -0800

    Update UI after addMessage mutation

--- src/components/AddMessage.js
+++ src/components/AddMessage.js
@@ -3,7 +3,42 @@ import { useMutation } from '@apollo/react-hooks';
 
 import Error from './shared/Error';
 
+import { GET_CHANNEL } from '../queries';
 import { ADD_MESSAGE } from '../mutations';
+
+const updateMessages = (
+  cache,
+  {
+    data: {
+      addMessage: message
+    }
+  },
+  channelId
+) => {
+  const data = cache.readQuery({
+    query: GET_CHANNEL,
+    variables: {
+      id: channelId
+    }
+  });
+
+  cache.writeQuery({
+    query: GET_CHANNEL,
+    variables: {
+      id: channelId
+    },
+    data: {
+      ...data,
+      channel: {
+        ...data.channel,
+        messages: [
+          ...data.channel.messages,
+          message
+        ]
+      }
+    }
+  });
+};
 
 const AddMessage = ({ channelId }) => {
   const [message, setMessage] = useState('');
@@ -14,7 +49,8 @@ const AddMessage = ({ channelId }) => {
         channelId,
         text: message
       }
-    }
+    },
+    update: (cache, result) => updateMessages(cache, result, channelId)
   });
 
   const onChange = e => setMessage(e.target.value);
